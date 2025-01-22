fde1c65ec37b616a0d0d690bd90044a2fca0e599
ukpono <titusukpono0@gmail.com>
2025-01-22 13:48:19 +0100

    updates

--- src/shared/components/assignments/tabledata/tabledata.tsx
+++ src/shared/components/assignments/tabledata/tabledata.tsx
@@ -1,26 +1,59 @@
 import { useState } from "react";
 import Modal from "../../common/modal/modal";
-// import { assignmentsData } from "@/core/const/tabledata";
+import { assignmentsData } from "@/core/const/tabledata";
+import { Assignment } from "@/core/types/interface/assignments";
+import { TableHeaderActionProp } from "@/core/types/interface/component/table";
 
-export default function AssignmentsTableActionComponent() {
-    const [open, setOpen]=useState(false)
-    const handleModalOpen=()=>{
-        setOpen(true)
-    }
-    const handleCloseModal=()=>{
-        setOpen(false)
-    }
-    // let submits = assignmentsData.find((items)=> items.submit  === items.submit)
+export default function AssignmentsTableActionComponent(props: TableHeaderActionProp<Assignment>) {
+  const [open, setOpen] = useState(false);
+  const [selectedAssignment, setSelectedAssignment] = useState<Assignment | null>(null);
+
+  const handleModalOpen = (assignment: Assignment) => {
+    setSelectedAssignment(assignment);
+    setOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpen(false);
+    setSelectedAssignment(null);
+  };
+
+  const isSubmissionAllowed = (assignment: Assignment) => {
+    const currentDate = new Date();
+    const dueDate = new Date(assignment.dueDate);
+    return currentDate <= dueDate || assignment.allowLateSubmission;
+  };
+
   return (
-    <div className="flex flex-row gap-4">
-       <span className="cursor-pointer "  onClick={handleModalOpen}>
-       {/* {submits} */}
-       </span>
-      <div>
-        <Modal isOpen={open} onClose={handleCloseModal } displayClose={true} title="Roles Configuration">
-            {/* <RoleconfigurationModal /> */}
-        </Modal>
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-row items-center gap-4">
+        <span
+          className={`cursor-pointer ${
+            isSubmissionAllowed(props.item!) ? "text-blue-500 underline" : "text-gray-400"
+          }`}
+          onClick={() => isSubmissionAllowed(props.item!) && handleModalOpen(props.item!)}
+        >
+          {isSubmissionAllowed(props.item!) ? props.item?.submit : "Submission Closed"}
+        </span>
       </div>
+
+      {open && selectedAssignment && (
+        <Modal isOpen={open} onClose={handleCloseModal} displayClose={true} title="Assignment Action">
+          {selectedAssignment.submit === "Upload" && (
+            <div>
+              <h3>Upload Assignment</h3>
+              <p>Due Date: {selectedAssignment.dueDate}</p>
+              {/* Add file upload inputs here */}
+            </div>
+          )}
+          {selectedAssignment.submit === "Submitted" && (
+            <div>
+              <h3>View Submitted Assignment</h3>
+              <p>Your assignment was already submitted.</p>
+            </div>
+          )}
+        </Modal>
+      )}
     </div>
   );
 }
