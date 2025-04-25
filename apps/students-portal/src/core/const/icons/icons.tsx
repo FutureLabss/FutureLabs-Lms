f7b090c9486097096f0062e377a3257cba37b9ae
PeroHub <peterime600@gmail.com>
2025-04-25 15:13:46 +0100

    fixed student side

--- apps/students-portal/src/core/const/icons/icons.tsx
+++ apps/students-portal/src/core/const/icons/icons.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
 import { GoPlus } from "react-icons/go";
 import { LuScanLine } from "react-icons/lu";
@@ -20,24 +21,64 @@ import { MdOutlineArrowRightAlt } from "react-icons/md";
 import { RiDeleteBin5Line } from "react-icons/ri";
 // import { FaTrashAlt } from "react-icons/fa";
 
-export const Link = (props: React.SVGProps<SVGSVGElement>) => <LuScanLine {...props} />;
-export const Qr = (props: React.SVGProps<SVGSVGElement>) => <MdQrCode2 {...props} />;
-export const Circle = (props: React.SVGProps<SVGSVGElement>) => <FiMinusCircle {...props} />;
-export const Message = (props: React.SVGProps<SVGSVGElement>) => <BiMessageDetail {...props} />;
-export const Hash = (props: React.SVGProps<SVGSVGElement>) => <LuHash {...props} />;
-export const Repeat = (props: React.SVGProps<SVGSVGElement>) => <IoRepeat {...props} />;
-export const Bin = (props: React.SVGProps<SVGSVGElement>) => <RiDeleteBin5Line {...props} />;
-export const Home = (props: React.SVGProps<SVGSVGElement>) => <CiHome {...props} />;
-export const Plus = (props: React.SVGProps<SVGSVGElement>) => <GoPlus {...props} />;
-export const Userg = (props: React.SVGProps<SVGSVGElement>) => <FaUsers {...props} />;
-export const Pencil = (props: React.SVGProps<SVGSVGElement>) => <HiOutlinePencilAlt {...props} />;
-export const Usercancel = (props: React.SVGProps<SVGSVGElement>) => <FaUserTimes {...props} />;
-export const Edit = (props: React.SVGProps<SVGSVGElement>) => <TiPencil {...props} />;
-export const SearchIcon = (props: React.SVGProps<SVGSVGElement>) => <FiSearch {...props} />;
-export const QuesCircle = (props: React.SVGProps<SVGSVGElement>) => <FaRegCircleQuestion {...props} />;
-export const UploadDel = (props: React.SVGProps<SVGSVGElement>) => <ImBin {...props} />;
-export const UploadRefresh = (props: React.SVGProps<SVGSVGElement>) => <LuRefreshCcw {...props} />;
-export const Copy = (props: React.SVGProps<SVGSVGElement>) => <GoCopy {...props} />;
-export const ArrowRight = (props: React.SVGProps<SVGSVGElement>) => <MdOutlineArrowRightAlt {...props} />;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const Link = (props: React.SVGProps<SVGSVGElement>) => (
+  <LuScanLine {...(props as any)} />
+);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const Qr = (props: React.SVGProps<SVGSVGElement>) => (
+  <MdQrCode2 {...(props as any)} />
+);
+export const Circle = (props: React.SVGProps<SVGSVGElement>) => (
+  <FiMinusCircle {...(props as any)} />
+);
+export const Message = (props: React.SVGProps<SVGSVGElement>) => (
+  <BiMessageDetail {...(props as any)} />
+);
+export const Hash = (props: React.SVGProps<SVGSVGElement>) => (
+  <LuHash {...(props as any)} />
+);
+export const Repeat = (props: React.SVGProps<SVGSVGElement>) => (
+  <IoRepeat {...(props as any)} />
+);
+export const Bin = (props: React.SVGProps<SVGSVGElement>) => (
+  <RiDeleteBin5Line {...(props as any)} />
+);
+export const Home = (props: React.SVGProps<SVGSVGElement>) => (
+  <CiHome {...(props as any)} />
+);
+export const Plus = (props: React.SVGProps<SVGSVGElement>) => (
+  <GoPlus {...(props as any)} />
+);
+export const Userg = (props: React.SVGProps<SVGSVGElement>) => (
+  <FaUsers {...(props as any)} />
+);
+export const Pencil = (props: React.SVGProps<SVGSVGElement>) => (
+  <HiOutlinePencilAlt {...(props as any)} />
+);
+export const Usercancel = (props: React.SVGProps<SVGSVGElement>) => (
+  <FaUserTimes {...(props as any)} />
+);
+export const Edit = (props: React.SVGProps<SVGSVGElement>) => (
+  <TiPencil {...(props as any)} />
+);
+export const SearchIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <FiSearch {...(props as any)} />
+);
+export const QuesCircle = (props: React.SVGProps<SVGSVGElement>) => (
+  <FaRegCircleQuestion {...(props as any)} />
+);
+export const UploadDel = (props: React.SVGProps<SVGSVGElement>) => (
+  <ImBin {...(props as any)} />
+);
+export const UploadRefresh = (props: React.SVGProps<SVGSVGElement>) => (
+  <LuRefreshCcw {...(props as any)} />
+);
+export const Copy = (props: React.SVGProps<SVGSVGElement>) => (
+  <GoCopy {...(props as any)} />
+);
+export const ArrowRight = (props: React.SVGProps<SVGSVGElement>) => (
+  <MdOutlineArrowRightAlt {...(props as any)} />
+);
 
 // export const Usercancel = (props: React.SVGProps<SVGSVGElement>) => <FaUserTimes  {...props} />;
