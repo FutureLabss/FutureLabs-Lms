d55236e9e6da604629e7cc7a65fb3366e4d1be61
goodybestie <goodnessbassey003@gmail.com>
2025-05-19 16:54:37 +0100

    classroom

--- apps/students-portal/src/pages/login.tsx
+++ apps/students-portal/src/pages/login.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import logo from "../assets/logo.png";
 import loginprofile from "../assets/loginprofile.png";
-import google from "../assets/google.png";
+// import google from "../assets/google.png";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import { useState } from "react";
 import { useAuthContext } from "@/shared/context/auth";
